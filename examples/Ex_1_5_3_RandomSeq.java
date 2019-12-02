559e436681a4578bba35a34e96fc23497144656e
Talgat-MSI <talgat.iskindir@gmail.com>
2019-12-02 20:35:36 +0600

    Example 1.5.3 Random Sequence

---
+++ examples/Ex_1_5_3_RandomSeq.java
@@ -0,0 +1,9 @@
+public class Ex_1_5_3_RandomSeq {
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(Math.random());
+        }
+    }
+}
